d41e5cd6cbb75bd3fb75edbc5b19806594e8090d
Inha921119 <dlsgk921119@gmail.com>
2023-02-14 15:30:31 +0900

    게시글 작성 기능 구현

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,40 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("== 프로그램 시작 ==");
-		
+
 		Scanner sc = new Scanner(System.in);
-		
-		while(true) {
+
+		int lastArticleId = 0;
+
+		while (true) {
 			System.out.printf("명령어) ");
 			String cmd = sc.nextLine();
-			
-			if (cmd.equals("exit")) {
+
+			if (cmd.length() == 0) {
+				System.out.println("명령어를 입력해 주세요.");
+				continue;
+			}
+
+			if (cmd.equals("article list")) {
+				if (lastArticleId == 0) {
+					System.out.println("게시글이 없습니다.");
+				}
+			} else if (cmd.equals("article write")) {
+				int id = lastArticleId + 1;
+				lastArticleId = id;
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+
+				System.out.printf("내용 : ");
+				String contents = sc.nextLine();
+
+				System.out.printf("%d번 글이 생성되었습니다.\n", id);
+			} else if (cmd.equals("exit")) {
 				break;
+			} else {
+				System.out.println("존재하지 않는 명령어 입니다.");
 			}
 		}
-
 		System.out.println("== 프로그램 끝 ==");
 
 		sc.close();
